6518e5e96efd05c6f91b61d9267acc30a556ef06
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-01-18 11:09:14 +0400

    composition api ref

---
+++ src/components/Data.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h2>Options - {{o_firstName}}</h2>
+        <h2>Composition - {{c_firstName}}</h2>
+        <h2>Composition - {{ greet }}</h2>
+    </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+    name: 'Data',
+    setup(){
+        const c_firstName = ref('Clarck')
+        c_firstName.value = 'Diana'
+        const greet = `Hello ${c_firstName.value}`
+        console.log('c_firstName', c_firstName)
+        return{
+            c_firstName,
+            greet,
+        }
+    },
+    data(){
+        return{
+            o_firstName: 'Bruce',
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
